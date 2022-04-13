5cb7ee4b78ab427918d2afb6adb71f94199d67c9
KHJ9 <34563973+KHJ9@users.noreply.github.com>
2022-04-13 19:12:40 +0900

    Delete HELLO_PYTHON directory

--- HELLO_PYTHON/day02/mytest02.py
+++
@@ -1,7 +0,0 @@
-# 출력할 단수를 입력하시오
-
-a = input("출력할 단수를 입력하시오 : ")
-aa = int(a)
-
-for i in range(1, 9+1) :
-    print("{} x {} = {}".format(aa , i , aa*i))
--- HELLO_PYTHON/day03/mydef02.py
+++
@@ -1,5 +0,0 @@
-def showdan(dan):
-    for i in range(1,9+1) :
-        print("{}*{}={}".format(dan,i,dan*i))
-
-showdan(5)
